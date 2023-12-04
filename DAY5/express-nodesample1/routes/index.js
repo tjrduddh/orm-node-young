1bee123bb60409ce99a7fc188ec6b84a33d28f9a
dhdudtjr <youngsuk50@gmail.com>
2023-12-04 18:04:19 +0900

    노드익스프레스 day5

---
+++ DAY5/express-nodesample1/routes/index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* 메인 웹페이지 요청 및 응답처리 라우팅 메소드 */
+//http://localhost:3000
+router.get('/', function(req, res, next) {
+  res.render('index.ejs', { title: '영석ee' });
+});
+
+module.exports = router;
