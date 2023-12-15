ae0ca3c6a9b4b8ca6f32375091da6037482cccdd
dhdudtjr <youngsuk50@gmail.com>
2023-12-15 17:29:30 +0900

    REST API & debugger

--- DAY13/nodeviewapp/app.js
+++ DAY13/nodeviewapp/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var articleRouter = require('./routes/article');
+var articleAPIRouter = require('./routes/articleAPI');
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/article', articleRouter);
-
+app.use('/api/article', articleAPIRouter);
 
 
 
